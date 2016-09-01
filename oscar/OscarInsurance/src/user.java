a2a798c1fd8a1175c9a3b375a27c56d20e7e3d79
SolarLunix <solarlunix@gmail.com>
2016-09-01 14:41:33 +0100

    Varialbles
    Added some necessary variables. Needs to be checked using eclipse as this was modified in browser.

--- oscar/OscarInsurance/src/user.java
+++ oscar/OscarInsurance/src/user.java
@@ -1,4 +1,11 @@
 
 public class user {
+       //Variables 
+       private String screenName;
+       private String firstName;
+       private String lastName;
+       private int age;
+       private Boolean admin;
+       private int policyNum;
 
 }
