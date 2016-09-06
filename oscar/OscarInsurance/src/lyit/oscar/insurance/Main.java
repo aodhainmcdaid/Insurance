28db21e5707cd09aa8b6a1ab49bcc6d751dd3624
SolarLunix <solarlunix@gmail.com>
2016-09-06 15:33:50 +0100

    Added Admin and Main file

---
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package lyit.oscar.insurance;
+
+/**
+ * @author Melissa Melaugh (SolarLunix)
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Run run = new Run();
+		run.run();
+		run.shutdown();
+		System.out.println("Goodbye");
+	}
+}
