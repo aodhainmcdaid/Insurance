dc2db83b4b899f59eceac8380d1589d7c327ca02
daniela <danielapricopie1967@gmail.com>
2016-09-07 13:39:39 +0100

    fixed

--- oscar/OscarInsurance/src/lyit/oscar/insurance/ConectionDBTrue.java
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/ConectionDBTrue.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-//import com.mysql.jdbc. Driver;
+//import com.mysql .jdbc. Driver;
 
 public class ConectionDBTrue {
 	public static void startDB() {
