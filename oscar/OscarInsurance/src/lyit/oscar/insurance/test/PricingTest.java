ffa99ba270f45253b7ba1f0acc1d0bc7b066f6e3
SolarLunix <solarlunix@gmail.com>
2016-09-07 09:21:04 +0100

    Fixed Packages I moved the Tests into their proper packages, deleted my note from before and re-added the JUnit 4 library to my branch, however, it appears that libraries that each person adds is not added to GitHub and so libraries will need to be added manually by each person.

---
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/test/PricingTest.java
@@ -0,0 +1,34 @@
+package lyit.oscar.insurance.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PricingTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPricing() {
+		fail("Not yet implemented");
+	}
+
+}
