d90f9ffeea7f4df599a856bbd45cbe9e12a5917c
SolarLunix <solarlunix@gmail.com>
2016-09-13 11:46:08 +0100

    Renamed Admin2 Admin

--- oscar/OscarInsurance/src/lyit/oscar/insurance/Admin.java
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/Admin.java
@@ -1,13 +1,13 @@
 package lyit.oscar.insurance;
 
-public class Admin2 extends User2 {
+public class Admin extends User2 {
 
 	
 	/*private String firstName;
 	private String lastName;*/
 	
 	
-	public Admin2(String first, String last) 
+	public Admin(String first, String last) 
 	{
 		super(first, last);
 	}
