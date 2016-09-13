b720eac613299f58eef30353e43ca09d732773ec
SolarLunix <solarlunix@gmail.com>
2016-09-13 13:08:07 +0100

    Updated the JUnit Test RunTest.java

--- oscar/OscarInsurance/src/lyit/oscar/insurance/test/RunTest.java
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/test/RunTest.java
@@ -1,14 +1,43 @@
 package lyit.oscar.insurance.test;
 
 import static org.junit.Assert.*;
+import lyit.oscar.insurance.Run;
 
 import org.junit.Test;
 
 public class RunTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void displaytest() {
+		System.out.println("Admin Display");
+		Run.displayAdminMenu();
+		
+		System.out.println("User Display");
+		Run.displayClientMenu();
+	}
+	
+	@Test
+	public void testStrInput(){
+		String str = Run.getStrResponse("Please type the letter a then hit enter");
+		assert(str.equalsIgnoreCase("a"));
+	}
+	
+	@Test
+	public void testNumInput(){
+		int anInt = Run.getNumResponse("Please select 7 then hit enter", 0, 10);
+		assertEquals(7, anInt);
+	}
+	
+	@Test
+	public void testCreateUser(){
+		//TODO: Create user test
+		fail("Not generated");
+	}
+	
+	@Test 
+	public void testRun(){
+		//TODO: Create Run Test
+		fail("Not generated");
 	}
 
 }
