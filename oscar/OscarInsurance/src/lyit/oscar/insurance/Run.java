8e68ce7e6f20db7cb4658ebc54cbb4c8cba4cce8
SolarLunix <solarlunix@gmail.com>
2016-09-13 12:12:11 +0100

    Updated Run Fixed spacing and indentation, and updated the createUser code.

--- oscar/OscarInsurance/src/lyit/oscar/insurance/Run.java
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/Run.java
@@ -15,9 +15,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-//Run2 created to account for problems in original
-
-
 public class Run {
 	
 	String AdminPass= "pass";
@@ -33,7 +30,7 @@ public class Run {
 		keyIn = new Scanner(System.in);
 		Connection conn = null;
 		System.out.println("Starting Connection to DB");
-		//conn = MYSQLconnect.getConnection();
+		conn = MYSQLconnect.getConnection();
 		
 		System.out.println("Created Connection \n");
 		int option;
@@ -46,107 +43,108 @@ public class Run {
 
 			switch(option)
 			{
-			case 1: //open account
-				displayClientMenu();	
-				int numb = getNumResponse("\nEnter option",0,2);                    
-
-				switch(numb)
-				{
-				case 1:                                           
-					User client = createUser();
-					User client2 = List.addUser(client);
-					PolicyDetails policy1 = List.getPolicyDetails();
-               System.out.println(" Your policy Number is: " +policy1.getPolicyID());
-					System.out.println("Added!");
-               System.out.println(policy1);
-               System.out.println(client2);
-               
-					//sqlEngine.insertCust(client2);          
-					//sqlEngine.insertPol(polNo);
-					
-					break;
-
-				case 2: 
-					String numb2 = getStrResponse("\nEnter your Policy Number: ");
-					User client3 = List.viewPolicy(numb2);
-					if(client3 != null)
+				case 1: //open account
+					displayClientMenu();	
+					int numb = getNumResponse("\nEnter option",0,2);                    
+	
+					switch(numb)
 					{
-						//User2 client4 = sqlEngine.selectCust(client3);
-                  System.out.println("start printing");
-						System.out.println(client3);
-						
-					}
-					else
+						case 1:                                           
+							User client = createUser();
+							User client2 = List.addUser(client);
+							PolicyDetails policy1 = List.getPolicyDetails();
+							System.out.println(" Your policy Number is: " +policy1.getPolicyID());
+							System.out.println("Added!");
+							System.out.println(policy1);
+							System.out.println(client2);
+		               
+							//sqlEngine.insertCust(client2);          
+							//sqlEngine.insertPol(polNo);
+							
+							break;
+		
+						case 2: 
+							String numb2 = getStrResponse("\nEnter your Policy Number: ");
+							User client3 = List.viewPolicy(numb2);
+							if(client3 != null)
+							{
+								//User2 client4 = sqlEngine.selectCust(client3);
+								System.out.println("start printing");
+								System.out.println(client3);
+								
+							}
+							else
+							{
+								System.out.println("Policy Number invalid..Can't view your details");
+							}
+							break;
+		
+						case 0: 	
+							System.out.println("Thank you..");
+							break;  
+		
+						default:	System.out.println("Invalid Customer option entered " );            
+					} //end switch 2                                        						
+					break;		
+	
+				case 2: //close account
+					displayAdminMenu();
+					int numb2 = getNumResponse("\nEnter option",0,3);                    
+	
+					switch(numb2)
 					{
-						System.out.println("Policy Number invalid..Can't view your details");
-					}
-					break;
-
+						case 1:
+		
+							String numb3 = getStrResponse("\nEnter Client Policy Number: ");
+							User client2 = List.viewPolicy(numb3);
+							if(client2 != null)
+							{
+								//User2 client3 = sqlEngine.selectCust(client2);
+								//List2.displayUser(client3);
+							}
+							else
+							{
+								System.out.println("Policy Number invalid..Can't view client details");
+							}                                 
+							break;
+		
+						case 2: 
+							break;
+		
+						case 3: 	
+							String numb4 = getStrResponse("\nEnter Client Policy Number: ");
+							User client4 = List.deleteUser(numb4);
+							if(client4 != null)
+							{
+								//sqlEngine.delCust(client4);
+							}
+							else
+							{
+								System.out.println("Policy Number is invalid..Can't delete client");
+							}                              
+							break;
+		
+						case 0: 	
+							System.out.println("Thank you Adminstrator..");
+							break;  
+		
+						default:	
+							System.out.println("Invalid  Admin option entered" );            
+					} //end switch 3                                        						
+					break;		              						
+	
 				case 0: 	
-					System.out.println("Thank you..");
+					System.out.println("Goodday..");
 					break;  
-
-				default:	System.out.println("Invalid Customer option entered " );            
-				} //end switch 2                                        						
-				break;		
-
-			case 2: //close account
-				displayAdminMenu();
-				int numb2 = getNumResponse("\nEnter option",0,3);                    
-
-				switch(numb2)
-				{
-				case 1:
-
-					String numb3 = getStrResponse("\nEnter Client Policy Number: ");
-					User client2 = List.viewPolicy(numb3);
-					if(client2 != null)
-					{
-						//User2 client3 = sqlEngine.selectCust(client2);
-						//List2.displayUser(client3);
-					}
-					else
-					{
-						System.out.println("Policy Number invalid..Can't view client details");
-					}                                 
-					break;
-
-				case 2: 
-					break;
-
-				case 3: 	
-					String numb4 = getStrResponse("\nEnter Client Policy Number: ");
-					User client4 = List.deleteUser(numb4);
-					if(client4 != null)
-					{
-						//sqlEngine.delCust(client4);
-					}
-					else
-					{
-						System.out.println("Policy Number is invalid..Can't delete client");
-					}                              
-					break;
-
-				case 0: 	
-					System.out.println("Thank you Adminstrator..");
-					break;  
-
+	
 				default:	
-					System.out.println("Invalid  Admin option entered" );            
-				} //end switch 3                                        						
-				break;		              						
-
-			case 0: 	
-				System.out.println("Goodday..");
-				break;  
-
-			default:	
-				System.out.println("Invalid option entered " );			
+					System.out.println("Invalid option entered " );			
 			}//end switch 1
 		
 		}while(option !=0);
 			
-	}		
+	}
+	
 	/**
 	 * 
 	 * @return display login for customer
@@ -185,88 +183,46 @@ public class Run {
 	static public int getNumResponse(String aString, int min, int max){
 		//variables
 		int response = min - 3; //make sure that the default response cannot be marked as valid
-		Scanner in = new Scanner(System.in);
 		Boolean noResponse = true;
 		 
 		//Ask the user for their input
 		while(noResponse){
-		System.out.println(aString);
-		try{
-		response = in.nextInt();
-		if(response >= min && response <= max){
-		noResponse = false; //You have a response if the given number is between the min and max given
-		}else{
-		System.out.println("Invalid Response.");
+			System.out.println(aString);
+			try{
+				response = keyIn.nextInt();
+				if(response >= min && response <= max){
+					noResponse = false; //You have a response if the given number is between the min and max given
+				}else{
+					System.out.println("Invalid Response.");
+				}
+			}catch(InputMismatchException e){
+				keyIn.next();
+				System.out.println("Bad Response");
+			}
 		}
-		}catch(InputMismatchException e){
-		in.next();
-		System.out.println("Bad Response");
-		}
-		}
-		 
-		//close the scanner down and return the response
 		return response;
-		}
+	}
 
-		static public String getStrResponse(String aString){
+	static public String getStrResponse(String aString){
 		String response = "";
-		Scanner input = new Scanner(System.in);
 		while(true){
-		System.out.println(aString);
-		response = input.next();
-		String hold = "Is " + response + " correct? \n\t1: Yes \n\t2: No";
-		int ans = getNumResponse(hold, 1, 2);
-		if(ans == 1){
-		break;
-		}
+			System.out.println(aString);
+			response = keyIn.next();
+			if(!response.isEmpty()){
+				break;
+			}
 		}
 		return response;
-		 
-		}
+	}
+	
 	/**
 	 * Creates a new user based on information that the user provides, and then updates the current user to the newly
 	 * created one from this method.
 	 */
-/*	public static User2 createUser(){
+	public static User createUser(){
+		User aNewUser = new User(null, null, null, null, null, null, null, null);
 		//Create the title name, and user's full name
-		String sn = getStrResponse("Please input your title then hit enter: ");
-		String first = getStrResponse("Please input your first name then hit enter: ");
-		String last = getStrResponse("Please input your last name then hit enter: ");
-		String phone = getStrResponse("Please input your last name then hit enter: ");
-		String email = getStrResponse("Please input your last name then hit enter: ");
-		String address = getStrResponse("Please input your last name then hit enter: ");
-		LocalDate birthday = LocalDate.now();
-
-		//Create the User's Birthday
 		while(true){
-			//Ask user for the year and month of their DOB
-			int year = getNumResponse("Please input the year you were born: ", 1900, LocalDate.now().getYear());
-			int month = getNumResponse("Please input the number of the month you were born: ", 1, 12);
-
-			//update birthday with the year and the month
-			birthday = LocalDate.of(year, month, 1);
-
-			//update max day to the length of the birth month so that the user cannot input a number outside of this range
-			int maxDay = birthday.lengthOfMonth();
-
-			//ask for the user's day
-			int day = getNumResponse("Please enter the day you were born: ", 1, maxDay);
-
-			//update Birthday
-			birthday = LocalDate.of(year, month, day);
-
-			String hold = "Is your birthday " + birthday.getDayOfMonth() + " of " + birthday.getMonth() + birthday.getYear() +"? "
-					+ "\n\t1: Yes \n\t2: No";
-			int response = getNumResponse(hold, 1, 2);
-
-			if(response == 1){
-				break;
-			}
-		}*/
-
-		public static User createUser(){
-			//Create the title name, and user's full name
-			
 			String title = getStrResponse("Please input your title: ");
 			String first = getStrResponse("Please input your first name: ");
 			String last = getStrResponse("Please input your last name: ");
@@ -276,38 +232,34 @@ public class Run {
 			
 			//String policyNo= "1";
 			LocalDate birthday = LocalDate.now();
-
+	
 			//Create the User's Birthday
 			while(true){
 				//Ask user for the year and month of their DOB
 				int year = getNumResponse("Please input the year you were born: ", 1900, LocalDate.now().getYear());
 				int month = getNumResponse("Please input the number of the month you were born: ", 1, 12);
-
+	
 				//update birthday with the year and the month
 				birthday = LocalDate.of(year, month, 1);
-
+	
 				//update max day to the length of the birth month so that the user cannot input a number outside of this range
 				int maxDay = birthday.lengthOfMonth();
-
+	
 				//ask for the user's day
 				int day = getNumResponse("Please enter the day you were born: ", 1, maxDay);
-
+	
 				//update Birthday
 				birthday = LocalDate.of(year, month, day);
-
+	
 				String hold = "Is your birthday " + birthday.getDayOfMonth() + " of " + birthday.getMonth() + birthday.getYear() +"? "
 						+ "\n\t1: Yes \n\t2: No";
 				int response = getNumResponse(hold, 1, 2);
-
+	
 				if(response == 1){
 					break;
 				}
 			}
-
-			/*//Create the user's password - note this should be turned into a 
-			String password = getStrResponse("Please enter a password then hit enter: ");
-			int hashedPassword = password.hashCode();
-	*/
+	
 			int gender = getNumResponse("Please chose you gender: \n\t1: Male \n\t2:Female", 1, 2);
 			String a_gender = "";
 			if(gender == 1){
@@ -315,55 +267,42 @@ public class Run {
 			}else{
 				a_gender = "Female";
 			}
-
-			//Choose policy Type 
-			//int polType = 2;
 			
+			//update user
+			aNewUser = new User(title, first, last, birthday, phone, a_gender, email, address);
 			
-			//Create the user based on the given information
-			//String polNum  = policyId();
-			User aNewUser = new User(title, first, last, birthday, phone, a_gender,email, address);
-			
-			// TODOwill have to work on the policy number. run 1 has nice codes. this is dummy
-			//Pricing priceUser = new Pricing(aNewUser); 
-			aNewUser.setPersonID();
-			aNewUser.getAge();
-			
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-					
-			System.out.print("Select Policy Type: ");
-			String type = getStrResponse("1)Partial Coverage 2)Full Coverage: ");
-			String startdate=getStrResponse("Enter Policy start Date in this format (yyyy-MM-dd): ");
-			LocalDate start =LocalDate.parse(startdate,dtf);
-			String enddate=getStrResponse("Enter Policy end Date in this format (yyyy-MM-dd):: ");
-			LocalDate end =LocalDate.parse(enddate,dtf);
-			String pay_type = getStrResponse("Enter payment type: ");
-			
-			
-			PolicyDetails aNewPolicy= new PolicyDetails(type, start, end, pay_type);
-			
-			aNewPolicy.generatePolicyID();
-        
-			List.addPolicy(aNewPolicy);
-         aNewUser.setPolicyNo(Integer.toString(aNewPolicy.getPolicyID()));
-			
-
-			
-			//List2.addUser(aNewUser);
-			//System.out.print("aaa");
-			return aNewUser;
+			int hold = getNumResponse(aNewUser.toString() + "/n/nIs the information above correct? \n\t1: Yes \n\t2: No", 1, 2);
+			if(hold == 1){
+				break;
+			}
 		}
 
-	// //TODO:Returns policy type
-// 	public static int policy()
-// 	{
-// 		return 0;
-// 	}
-// 	//TODO
-// 	public static String policyId()
-// 	{
-// 		return "555555";
-// 	}
-// 
+		//Choose policy Type 
+		//int polType = 2;
+		
+		//Pricing priceUser = new Pricing(aNewUser); 
+		aNewUser.setPersonID();
+		aNewUser.getAge();
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+				
+		System.out.print("Select Policy Type: ");
+		String type = getStrResponse("1)Partial Coverage 2)Full Coverage: ");
+		String startdate=getStrResponse("Enter Policy start Date in this format (yyyy-MM-dd): ");
+		LocalDate start =LocalDate.parse(startdate,dtf);
+		String enddate=getStrResponse("Enter Policy end Date in this format (yyyy-MM-dd):: ");
+		LocalDate end =LocalDate.parse(enddate,dtf);
+		String pay_type = getStrResponse("Enter payment type: ");
+		
+		
+		PolicyDetails aNewPolicy= new PolicyDetails(type, start, end, pay_type);
+		
+		aNewPolicy.generatePolicyID();
+    
+		List.addPolicy(aNewPolicy);
+		aNewUser.setPolicyNo(Integer.toString(aNewPolicy.getPolicyID()));
+		
+		return aNewUser;
+	}
 
 }
