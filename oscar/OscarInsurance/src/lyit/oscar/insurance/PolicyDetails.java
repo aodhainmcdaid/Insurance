2e5bbf835dd0330984dd83922d3f8f15a3ba8f0f
SolarLunix <SolarLunix@gmail.com>
2016-09-14 20:03:10 +0100

    Update JavaDoc PolicyDetails

--- oscar/OscarInsurance/src/lyit/oscar/insurance/PolicyDetails.java
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/PolicyDetails.java
@@ -1,21 +1,14 @@
-/**
- * @author Melissa and Daniela
-
- * 	Aodhain McDaid aaaa
- *  Fred Manu
- */
-
 package lyit.oscar.insurance;
-
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Date;
 import java.util.Random;
 
-
-
-
+/**
+ * This class's only function is to start the app and call the run class.
+ * @author Melissa, Daniela, Aodhain, and Fred
+ */
 public class PolicyDetails {
 
 	private String type;
@@ -24,94 +17,143 @@ public class PolicyDetails {
 	private String payment;
 	private int policyID;
 	private double cost;
-	private static int counter=5;
 	
-	
-	
-	
+	/**
+	 * 
+	 * @param policyType
+	 * @param policyStart
+	 * @param policyEnd
+	 * @param paymentType
+	 */
 	public PolicyDetails(String policyType, LocalDate policyStart, LocalDate policyEnd, String paymentType)
 	{
 		this.type=policyType;
 		this.start=policyStart;
 		this.end=policyEnd;
 		this.payment=paymentType;
-			
 	}
 	
+	/**
+	 * Sets the policy type to the given string
+	 * @param PolicyIn	the type of the policy
+	 */
 	public void setPolicyType(String PolicyIn)
 	{
 		this.type=PolicyIn;
-		
 	}
 	
+	/**
+	 * Gets the string value of the policy type	
+	 * @return	The policy type in String format
+	 */
 	public String getPolicyType()
 	{
 		return type;
-		
-		
 	}
+	
+	/**
+	 * Gets the price of the policy object as a double
+	 * @return	the price the current policy costs as a double
+	 */
 	public double getPolicyCost()
 	{
 		return cost;
 	}
+	
+	/**
+	 * Sets the price of the current policy
+	 * @param costIn	The price of a the current policy
+	 */
 	public void setPolicyCost(double costIn)
 	{
 		cost = costIn;
 	}
 	
+	/**
+	 * This sets the date the policy will be valid from
+	 * @param startIn	the date that the policy starts on 
+	 */
 	public void setPolicyStart(LocalDate startIn)
 	{
-		
 		this.start=startIn;
-		
 	}
+	
+	/**
+	 * This returns the date as a LocalDate 
+	 * @return	the date that the policy is valid from
+	 */
 	public LocalDate getPolicyStart()
 	{
 		return start;
-		
-		
 	}
+	
+	/**
+	 * This method sets the policy end date
+	 * @param endIn	The date that the policy if valid to
+	 */
 	public void setPolicyEnd(LocalDate endIn)
 	{
 		this.end=endIn;
-		
-		
 	}
+	
+	/**
+	 * This method returns the policy end date
+	 * @return	the date that the policy is valid to
+	 */
 	public LocalDate getPolicyEnd()
 	{
 		return end;
-		
 	}
+	
+	/**
+	 * This sets the payment type entered by the customer
+	 * @param paymentIn	The string value entered for the payment
+	 */
 	public void setPaymentType(String paymentIn)
 	{
 		this.payment=paymentIn;
-		
 	}
+	
+	/**
+	 * This method returns the payment type
+	 * @return	the string value of the payment type
+	 */
 	public String getPaymentType()
 	{
 		return payment;
-		
 	}
+	
+	/**
+	 * This method returns the policy ID as an int
+	 * @return	the policy ID
+	 */
 	public int getPolicyID()
 	{
 		return policyID;
-		
-		
 	}
+	
+	/**
+	 * This method sets the policy ID
+	 * @param policyIDIn	the unique policy ID
+	 */
 	public void setPolicyID(int policyIDIn)
 	{
 		policyID = policyIDIn;
-		
 	}
-   public String toString()
-   {
-      String str = "";
-      str = str + getPolicyID()+ "\n";
-      str = str + getPolicyType()+"\n";
-      str = str + getPolicyStart()+"\n";
+	
+	/**
+	 *  This method returns the policy information as a string.
+	 *  @return	the full policy details as a String
+	 */
+	public String toString()
+	{
+	   String str = "";
+	   str = str + getPolicyID()+ "\n";
+	   str = str + getPolicyType()+"\n";
+	   str = str + getPolicyStart()+"\n";
 	   str = str + getPolicyEnd()+"\n";
 	   str = str + getPaymentType();
       
-      return str;
-   }	
+	   return str;
+	}	
 }
