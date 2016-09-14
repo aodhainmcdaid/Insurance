3b2d73a56625598d2a1c621c489df6127513a322
Melissa Melaugh <solarlunix@gmail.com>
2016-09-14 13:09:19 +0100

    Update JavaDocs Admin

--- oscar/OscarInsurance/src/lyit/oscar/insurance/Admin.java
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/Admin.java
@@ -1,12 +1,15 @@
 package lyit.oscar.insurance;
 
+/**
+ * Authors: Melissa, Aodhain, Fred,  and Daniela
+ */
 public class Admin extends User {
-
-	
 	/*private String firstName;
 	private String lastName;*/
 	
-	
+	/**
+	 * This 
+	 */
 	public Admin(String first, String last) 
 	{
 		super(first, last);
