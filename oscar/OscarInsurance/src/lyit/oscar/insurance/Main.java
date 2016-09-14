1f495a5277cbd238efb651b14f58ed7442b4907d
SolarLunix <SolarLunix@gmail.com>
2016-09-14 13:20:51 +0100

    Updated JavaDocs Main

--- oscar/OscarInsurance/src/lyit/oscar/insurance/Main.java
+++ oscar/OscarInsurance/src/lyit/oscar/insurance/Main.java
@@ -1,22 +1,14 @@
-/**
- * @author Melissa and Daniela
- * 	Aodhain McDaid
- *  Fred Manu
- */
-	
-
 package lyit.oscar.insurance;
 
-import java.util.*;
-
+/**
+ * This class's only function is to start the app and call the run class.
+ * @author Melissa, Daniela, Aodhain, and Fred
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		System.out.println("starting App.....");
 		run.run3();
-
 		System.out.println("Ending App.....");
 	}
 }
